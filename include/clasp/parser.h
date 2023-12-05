bc1af0344d013462d2a6aa9bb687c10622aa03a6
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-12-05 10:09:42 -0500

    comit

--- include/clasp/parser.h
+++ include/clasp/parser.h
@@ -4,12 +4,40 @@
 #include <clasp/lexer.h>
 #include <stdio.h>
 
+typedef void (*ClaspOutputFn)(ClaspToken *token, FILE *out);
+
+typedef ClaspOutputFn ClaspVisitorTable[1];
+
+typedef enum {
+    VISITOR_BINOP,
+} ClaspVisitorTypes;
+
+void clasp_visit_binop(ClaspToken *token, FILE *out);
+
+const ClaspVisitorTable claspDefaultVisitors = {
+    [VISITOR_BINOP] = &clasp_visit_binop,
+};
+
 typedef struct {
     ClaspLexer *lexer;
-
     FILE *out;
+    const ClaspVisitorTable *visitors;
 } ClaspParser;
 
 ClaspParser *new_parser(ClaspLexer *lexer, FILE *out);
+ClaspParser *new_parser_custom(ClaspLexer *lexer, FILE *out, ClaspVisitorTable *visitors);
+
+void parser_compile(ClaspParser *parser);
+
+/**
+ * Order of operations:
+ * no
+*/
+
+void parser_expression(ClaspParser *parser);  // Expression statement
+void parser_term(ClaspParser *parser);        // Add/sub
+
+void parser_primary(ClaspParser *parser);     // Primary (parenthesis, numbers, ... stuff)
+
 
 #endif // PARSER_H
--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,26 @@
 #include <clasp/parser.h>
+#include <stdlib.h>
+
+ClaspParser *new_parser(ClaspLexer *l, FILE *out) {
+    ClaspParser *ret = malloc(sizeof(ClaspParser));
+    ret->lexer = l;
+    ret->out = out;
+    ret->visitors = &claspDefaultVisitors;
+
+    return ret;
+}
+ClaspParser *new_parser_custom(ClaspLexer *l, FILE *out, ClaspVisitorTable *v) {
+    ClaspParser *ret = malloc(sizeof(ClaspParser));
+    ret->lexer = l;
+    ret->out = out;
+    ret->visitors = v;
+
+    return ret;
+}
+
+void parser_compile(ClaspParser *p) {
+    parser_expression(p); // TODO: declstatements
+}
+void parser_expression(ClaspParser *p) {
+    parser_term(p);
+}
