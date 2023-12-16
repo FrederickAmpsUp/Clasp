4dfb2128e29e2d84e1677638bdd453c924c0f5f5
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-12-15 21:04:24 -0500

    Actually remove dynbuf library

--- src/dynbuf/dynbuf_clear.c
+++
@@ -1,6 +0,0 @@
-#include "dynbuf.h"
-
-void		dynbuf_clear(t_dynbuf *buf)
-{
-	buf->offset = 0;
-}
--- src/dynbuf/dynbuf_write.c
+++
@@ -1,6 +0,0 @@
-#include "dynbuf.h"
-
-int			dynbuf_write(t_dynbuf *buf, const void *data, int datasz)
-{
-	return (dynbuf_insert(buf, -1, data, datasz));
-}
