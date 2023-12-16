e30d343997da645398c5cbf0bf392588fb1d8936
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-12-15 21:03:58 -0500

    Remove dynbuf library, add licensing

--- include/clasp/print_ast.h
+++ include/clasp/print_ast.h
@@ -1,3 +1,28 @@
+/**
+ * Clasp AST printer declaration
+ * Authored 12/15/2023-present
+ * 
+ * This program is part of the Clasp Header Libraries
+ * 
+ * Copyright (c) 2023, Frederick Ziola
+ *                      frederick.ziola@gmail.com
+ * 
+ * SPDX-License-Identifier: GPL-3.0
+ * 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef PRINT_AST_H
 #define PRINT_AST_H
 
--- src/print_ast.c
+++ src/print_ast.c
@@ -11,12 +11,12 @@ void *claspPrintBinop(ClaspASTNode *binop) {
 }
 void *claspPrintUnop(ClaspASTNode *unop) {
     printf("(unnop: op=%s right=", unop->data.unop.op->data);
-    visit(unop->data.binop.right, clasp_ast_printer);
+    visit(unop->data.unop.right, clasp_ast_printer);
     printf(")");
     return NULL;
 }
 void *claspPrintNumLiteral(ClaspASTNode *lit) {
-    printf("(num_literal: val=%s)", lit->data.lit_num.value);
+    printf("(num_literal: val=%s)", lit->data.lit_num.value->data);
     return NULL;
 }
 
