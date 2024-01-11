64fc12104ee4e3ee91ee1994a0a0c306cf6c5695
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2024-01-11 08:41:21 -0500

    Added template visitor

---
+++ target/template_visitor.c
@@ -0,0 +1,79 @@
+/**
+ * Clasp Template AST visitor target implementing the target API
+ * Authored 1/11/2024
+ * 
+ * This program is part of the Clasp Source Libraries
+ * 
+ * Copyright (c) 2024, Frederick Ziola
+ *                      frederick.ziola@gmail.com
+ * 
+ * SPDX-License-Identifier: GPL-3.0
+ * 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <clasp/clasp.h>
+#include <clasp/visitor.h>
+
+void *visit_binop(ClaspASTNode *binop, void *args) {
+
+}
+
+void *visit_unop(ClaspASTNode *unop, void *args) {
+
+}
+
+void *visit_postfix(ClaspASTNode *postfix, void *args) {
+    
+}
+
+void *visit_lit_num(ClaspASTNode *lit, void *args) {
+
+}
+
+void *visit_var_ref(ClaspASTNode *var, void *args) {
+
+}
+
+void *visit_fn_call(ClaspASTNode *fn, void *args) {
+
+}
+
+void *visit_expr_stmt(ClaspASTNode *expr, void *args) {
+
+}
+
+void *visit_block_stmt(ClaspASTNode *block, void *args) {
+
+}
+
+void *visit_var_decl(ClaspASTNode *var, void *args) {
+
+}
+
+void *visit_fn_decl(ClaspASTNode *fn, void *args) {
+
+}
+
+void *visit_if(ClaspASTNode *stmt, void *args) {
+
+}
+
+void *visit_while(ClaspASTNode *stmt, void *args) {
+
+}
+
+void *visit_single_type(ClaspASTNode *type, void *args) {
+    
+}
