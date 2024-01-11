b78374166343cfde49c35335ba33c291a14bf6aa
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2024-01-11 08:44:30 -0500

    lol

--- lines.sh
+++ lines.sh
@@ -1,2 +1,2 @@
 # ugh, you're a monster
-find ./src/** ./include/clasp/** -not -path "./src/extern/*" -type f \( -name "*.c" -o -name "*.h" \) -exec cat {} \; | wc -l
+find ./target/** ./src/** ./include/clasp/** -not -path "./src/extern/*" -type f \( -name "*.c" -o -name "*.h" \) -exec cat {} \; | wc -l
